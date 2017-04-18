c4b9802176f448b85f0c6395dd4e800846a78f0f
praveenksarathi <kpraveen15703@gmail.com>
2017-04-18 18:46:07 +0530

    Update maven_app_installation.sh

--- maven_app_installation.sh
+++ maven_app_installation.sh
@@ -2,7 +2,8 @@
 sudo apt-get -f install
 sudo apt-get -y install git
 sudo git clone https://github.com/steinsag/gwt-maven-example
-sudo cd gwt-maven-example
+alias cdproj='cd gwt-maven-example'
+cdproj
 sudo apt-get -y install maven
 sudo mvn clean install
 mvn tomcat7:run-war-only
