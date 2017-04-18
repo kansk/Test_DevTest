79882b9aef88aa91ab2166fa9d5be56ff35c495e
praveenksarathi <kpraveen15703@gmail.com>
2017-04-18 19:13:51 +0530

    Update maven_app_installation.sh

--- maven_app_installation.sh
+++ maven_app_installation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get -f install
 sudo apt-get -y install git
+git config --global http.proxy ""
+git config --global https.proxy ""
 git clone https://github.com/steinsag/gwt-maven-example
 alias cdproj='cd gwt-maven-example'
 cdproj
